30ccaecf762a77518246b661636a998e3b07e461
ArturoAlvarezY <arturoavilamarketing@gmail.com>
2024-06-30 12:07:49 +0200

    finish te lvl 2!

--- app.js
+++ app.js
@@ -1,67 +1,66 @@
-/*
-500 gramos de harina de trigo
-300 ml de agua tibia
-2 cucharadas de aceite de oliva
-1 cucharadita de azúcar
-1 cucharadita de sal
-7 gramos de levadura seca (un sobre)
-
-*/
-
-
-
 let eggs= '3 huevos,';
 let wheatFlour= '500 gr de harina,';
 let water='300 ml de agua,';
 let suggar='1 cucharada de azucar,';
 let dryYeast= '7 cucharadas de levaduras,';
 let salt= '1 cucharada de sal';
+let mass='1. Para hacer la masa, debemos mezclar: ';
 
-let mass='1. Para hacer la masa, debemos mezclar: '
-
-
-const mixingMass=(str)=>{
+const mixingMass=()=>{
     let readyMass;
-    readyMass=(mass+  eggs+ ' ' + wheatFlour+ ' ' + ' '  + water + ' '+ suggar+ ' '+ dryYeast+ ' ' + salt);
+    readyMass= ` \n \n ${mass} ${eggs} ${wheatFlour} ${water} ${suggar} ${dryYeast} ${salt}`;
     return readyMass;
 }
-console.log(mixingMass())
-   /*
-   Segundo paso, 
-
-Salsa, Vierte aproximadamente 1/2 taza de salsa de tomate en el centro de la masa estirada.   
-Queso, Espolvorea de 1 a 1 1/2 tazas de queso mozzarella rallado sobre la salsa, asegurándote de cubrir la superficie de manera uniforme.
-Tomates, usa de 1 a 2 tomates medianos.
-*/
 
 let sauce= '1/2 tazas de salsa de tomate';
-let cheese= '1/2 taza de queso'
+let cheese= '1/2 taza de queso';
 let tomatoes= '2 tomates medianos';
-let onTheMass='2. En la masa, agrega los siguientes ingredientes: '
+let onTheMass='2. En la masa, agrega los siguientes ingredientes: ';
 
 const addInTheMass=()=>{
     let verting;
-    verting= (onTheMass+ ' ' + ' ' + sauce + ' ' + cheese+ ' '+ tomatoes)
+    verting= `\n \n ${onTheMass} ${sauce} ${cheese} ${tomatoes}`;
     return verting;
 }
 
-console.log(addInTheMass());
-
-/* horno 
-
-temperatura: 482°F;
-tiempo: 15 minutos;
-
-*/
-
 let time= '15 minutos';
 let temp= 'a una temperatura de 482°F';
-let oven='Una vez listo el paso anterior, poner en el horno durante'
-
+let oven='Una vez listo el paso anterior, poner en el horno durante';
 const bringToTheOven=()=>{
     let cooking;
-    cooking= oven+ ' ' + time+ ' '+ temp;
+    cooking= `\n \n 3. ${oven} ${time} ${temp}`;
     return cooking;
 }
 
-console.log(bringToTheOven());
+let pizzaChoping= '4. Pica la pizza con un cortador de pizzas';
+let dish= 'prepara tus platos para servir la pizza';
+let enjoy= 'y por ultimo, disfruta crack';
+
+const enjoyPizza = () =>
+{
+let justEnjoy;
+justEnjoy= `\n \n ${pizzaChoping}, ${dish}, ${enjoy}`;
+return justEnjoy
+};
+
+const pepperoni=()=>{
+
+    return `${mixingMass()} ${addInTheMass()} ${bringToTheOven()} ${enjoyPizza()}`;
+}
+
+
+const prompt = require('prompt-sync')();
+
+const Pizza = prompt('¿Cuál Pizza quieres mi panita? ');
+
+if(Pizza==='pepperoni'){
+    console.log('\n Preparemos la pizza juntos!');
+    console.log(`${pepperoni()}`);
+}
+else{
+   console.log('te puedo dar unas arepas mano') 
+}
+
+
+
+
